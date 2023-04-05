80f9d497b04fcae899e994386e121e01f63cee30
mlystopad <mlystopadt@gmail.com>
2023-04-05 13:39:22 +0200

    This commit doesnt exactly work, GET method to API works, however, I have not yet figured out, how i will handle object creation from Json. Imported Gson, Okhttp3 to the project, User-Agent header "FHMDB/1.0" is being added to request

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,17 +1,51 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
+import at.ac.fhcampuswien.fhmdb.MovieAPI;
+import com.google.gson.Gson;
+
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Movie implements Comparable<Movie>{
+    private final String iD;
     private final String title;
     private final String description;
     private final List<String> genre;
+    private final int releaseYear;
+    private final String imgURL;
+    private final int lengthInMin;
+    private final ArrayList<String> directors;
+    private final ArrayList<String> writers;
+    private final ArrayList<String> mainCast;
+    private final double rating;
     // TODO add more properties here
 
-    public Movie(String title, String description, List<String> genre) {
+    public Movie(String iD,String title, String description, List<String> genre, int releaseYear, String imgURL, int lengthInMin, ArrayList<String> directors, ArrayList<String> writers, ArrayList<String> mainCast, double rating) {
+        this.iD = iD;
         this.title = title;
         this.description = description;
         this.genre = genre;
+        this.releaseYear = releaseYear;
+        this.imgURL = imgURL;
+        this.lengthInMin = lengthInMin;
+        this.directors = directors;
+        this.writers = writers;
+        this.mainCast = mainCast;
+        this.rating = rating;
+    }
+
+    public Movie() {
+        this.iD = null;
+        this.title = null;
+        this.description = null;
+        this.genre = null;
+        this.releaseYear = 0;
+        this.imgURL = null;
+        this.lengthInMin = 0;
+        this.directors = null;
+        this.writers = null;
+        this.mainCast = null;
+        this.rating = 0;
     }
 
     public String getTitle() {
@@ -30,54 +64,10 @@ public class Movie implements Comparable<Movie>{
 
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
-        movies.add(new Movie("Avatar",
-                "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
-                Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.SCIENCEFICTION, Genre.FANTASY, Genre.MYSTERY)));
-        movies.add(new Movie("Equilibrium",
-                "In an oppressive future where all forms of feeling are illegal, a man in charge of enforcing the law rises to overthrow the system and state.",
-                Arrays.asList(Genre.ACTION, Genre.DRAMA, Genre.THRILLER, Genre.SCIENCEFICTION)));
-        movies.add(new Movie("Megamind",
-                "Evil genius Megamind finally defeats his do-gooder nemesis, Metro Man, but is left without a purpose in a superhero-free world.",
-                Arrays.asList(Genre.ACTION, Genre.COMEDY, Genre.SCIENCEFICTION, Genre.ANIMATION, Genre.FAMILY)));
-        movies.add(new Movie("The Dark Knight",
-                "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-                Arrays.asList(Genre.ADVENTURE, Genre.ACTION, Genre.THRILLER, Genre.CRIME, Genre.DRAMA, Genre.MYSTERY)));
-        movies.add(new Movie("Pulp Fiction",
-                "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-                Arrays.asList(Genre.COMEDY, Genre.CRIME, Genre.THRILLER, Genre.CRIME)));
-        movies.add(new Movie("Spirited Away",
-                "During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.",
-                Arrays.asList(Genre.ANIMATION, Genre.ADVENTURE, Genre.FANTASY, Genre.MYSTERY)));
-        movies.add(new Movie("Inglourious Basterds",
-                "In Nazi-occupied France during World War II, a plan to assassinate Nazi leaders by a group of Jewish U.S. soldiers coincides with a theatre owner's vengeful plans for the same.",
-                Arrays.asList(Genre.WAR, Genre.ACTION, Genre.COMEDY, Genre.DRAMA, Genre.ADVENTURE)));
-        movies.add(new Movie("Good Will Hunting",
-                "Will Hunting, a janitor at M.I.T., has a gift for mathematics, but needs help from a psychologist to find direction in his life.",
-                Arrays.asList(Genre.DRAMA, Genre.ROMANCE)));
-        movies.add(new Movie("12 Years a Slave",
-                "In the antebellum United States, Solomon Northup, a free black man from upstate New York, is abducted and sold into slavery.",
-                Arrays.asList(Genre.DRAMA, Genre.HISTORY)));
-        movies.add(new Movie("Forrest Gump",
-                "Forrest, a man with low IQ, recounts the early years of his life when he found himself in the middle of key historical events. All he wants now is to be reunited with his childhood sweetheart, Jenny.",
-                Arrays.asList(Genre.DRAMA, Genre.ROMANCE, Genre.COMEDY)));
-        movies.add(new Movie("The Shining",
-                "A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence, while his psychic son sees horrific forebodings from both past and future.",
-                Arrays.asList(Genre.MYSTERY, Genre.HORROR)));
-        movies.add(new Movie("Moulin Rouge",
-                "A poor Bohemian poet in 1890s Paris falls for a beautiful courtesan and nightclub star coveted by a jealous duke.",
-                Arrays.asList(Genre.MUSICAL, Genre.DRAMA, Genre.COMEDY, Genre.ROMANCE)));
-        movies.add(new Movie("Green Book",
-                "A working-class Italian-American bouncer becomes the driver for an African-American classical pianist on a tour of venues through the 1960s American South.",
-                Arrays.asList(Genre.BIOGRAPHY, Genre.COMEDY, Genre.DRAMA)));
-        movies.add(new Movie("Girl in the Picture",
-                "A young mother's mysterious death and her son's subsequent kidnapping open a decades-long mystery.",
-                Collections.singletonList(Genre.DOCUMENTARY)));
-        movies.add(new Movie("Once Upon a Time in the West",
-                "A mysterious stranger with a harmonica joins forces with a notorious desperado to protect a beautiful widow from a ruthless assassin working for the railroad.",
-                Arrays.asList(Genre.WESTERN, Genre.ACTION, Genre.ADVENTURE, Genre.DRAMA)));
-        movies.add(new Movie("Hustle",
-                "A basketball scout discovers a phenomenal street ball player while in Spain and sees the prospect as his opportunity to get back into the NBA.",
-                Arrays.asList(Genre.SPORT, Genre.DRAMA)));
+        MovieAPI movieAPI = new MovieAPI();
+
+
+
 
         return movies;
     }
